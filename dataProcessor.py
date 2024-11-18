c699ca95a61b817a684b2cee71d7f3fe361a617c
Eli Young <eliyoung4now@gmail.com>
2024-11-18 05:13:21 -0500

    added backoff for error handled call

--- dataProcessor.py
+++ dataProcessor.py
@@ -860,7 +860,7 @@ def main():
                     
                     with open('playByPlayErrors.txt', 'a') as f:
                         f.write(error_msg)
-                        time.sleep(1.83) #for just this one we will sleep for longer to not get rate limited
+                        time.sleep(1.84) #for just this one we will sleep for longer to not get rate limited
                         continue
                 print("processed game")
         print(f"Total neg at {year}:" + str(yearAnalyzer.total_negative_minutes))
