2a98e5aee414491a6fd75a486e805efe5fedf277
Eli Young <eliyoung4now@gmail.com>
2024-12-05 22:28:38 -0500

    added 2022 data, didnt parse yet, rate limited?

--- dataProcessor.py
+++ dataProcessor.py
@@ -955,7 +955,7 @@ def main():
 
     #VITAL, only commented for a sec for testing
     # for year in range(2000, 2025):
-    for year in range(2020, 2025):
+    for year in range(2022, 2025):
 
         attemptCounter = minToAttemptsClass()
         attempt_counter_file = os.path.join('dataForEachYear', f'attempt_counter_{year-1}-{year}.txt')
